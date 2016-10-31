31ebb4312edf44b9c79a4cc0f1aac713f7a02214
Milan Lempera <milanlempera@gmail.com>
2016-10-31 09:02:29 +0100

    part 1 - simple detail component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,54 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'Angular 2 worshop!';
+
+  session = {
+    "id": 400,
+    "title": "REST API je mrtvé, ať žije GraphQL",
+    "description": "REST se stal standardem pro API. Poslední dobou se ale ukazuje, že není vhodné pro uživatelské rozhraní. Facebook nám nabídl GraphQL a skvělou knihovnu Relay, která usnadňuje a zrychluje práci s daty v aplikacích. ",
+    "speakers": [
+      {
+        "id": 6,
+        "featured": false,
+        "name": "Petr Ferschmann",
+        "company": "puzzlette",
+        "country": "Pilsen, Czech Republic",
+        "photoUrl": "/images/people/petr_fershmann.jpg",
+        "shortBio": "Co-founded and operated cloud based accounting system ABRA @FlexiBee. Loves backend and so he is involved in @fnxio. Now he's starting a new project.",
+        "bio": "Co-founded and operated cloud based accounting system ABRA @FlexiBee. Loves backend and so he is involved in @fnxio. Now he's starting a new project.",
+        "tags": [
+          "Backend",
+          "REST API",
+          "GraphQL"
+        ],
+        "badges": [],
+        "socials": [
+          {
+            "icon": "twitter",
+            "name": "Twitter",
+            "link": "https://twitter.com/fersman"
+          },
+          {
+            "icon": "website",
+            "name": "Website",
+            "link": "http://www.ferschmann.cz"
+          },
+          {
+            "icon": "linkedin",
+            "name": "LinkedIn",
+            "link": "https://linkedin.com/in/fersman"
+          }
+        ]
+      }
+    ],
+    "language": "Czech",
+    "complexity": "Advanced",
+    "tags": [
+      "Backend",
+      "Web",
+      "GraphQL",
+      "REST API"
+    ]
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { DetailComponent } from './session/detail/detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DetailComponent
   ],
   imports: [
     BrowserModule,

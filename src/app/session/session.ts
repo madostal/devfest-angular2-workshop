71a520cf5d411095416776d6eb1f93f9bfbe47e6
Milan Lempera <milanlempera@gmail.com>
2016-11-02 08:49:01 +0100

    sync session type and tests

--- src/app/session/session.ts
+++ src/app/session/session.ts
@@ -3,6 +3,7 @@ import { Speaker } from "./speaker";
 export interface Session {
   id: number;
   title: string;
+  description: string,
   tags: string[];
   speakers?: Speaker[];
   language?: string;

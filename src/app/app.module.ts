09f660880e11cf9819d23b1200881b76e84ab522
Milan Lempera <milanlempera@gmail.com>
2016-11-02 08:35:37 +0100

    remove unused code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,11 @@ import { NgModule } from "@angular/core";
 import { ReactiveFormsModule } from "@angular/forms";
 import { HttpModule } from "@angular/http";
 import { AppComponent } from "./app.component";
-import { Error404Component } from "./error/error404/error404.component";
-import { SpeakerNamesPipe } from './session/speaker-names.pipe';
 import { SessionDetailCardComponent } from './session/session-detail-card/session-detail-card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    Error404Component,
-    SpeakerNamesPipe,
     SessionDetailCardComponent
   ],
   imports: [
